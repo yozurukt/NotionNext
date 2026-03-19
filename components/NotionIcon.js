7aa97756d1c9b1c3a729062f103eee96f6bc33b7
yozuru <d1322325603@gmail.com>
2026-03-19 20:06:07 +0800

    test

--- components/NotionIcon.js
+++ components/NotionIcon.js
@@ -3,6 +3,7 @@ import LazyImage from './LazyImage'
 /**
  * notion的图标icon
  * 可能是emoji 可能是 svg 也可能是 图片
+ * 也支持 Font Awesome class 字符串，如 `fas fa-code`、`far fa-star`、`fab fa-github`
  * @returns
  */
 const NotionIcon = ({ icon }) => {
@@ -12,6 +13,11 @@ const NotionIcon = ({ icon }) => {
 
   if (icon.startsWith('http') || icon.startsWith('data:')) {
     return <LazyImage src={icon} className='w-8 h-8 my-auto inline mr-1'/>
+  }
+
+  // 识别 Font Awesome class 字符串，如 `fas fa-code`、`far fa-star`、`fab fa-github` 等
+  if (/^(fas|far|fab|fal|fat|fa-) /i.test(icon) || /^fa[srbt]? fa-/i.test(icon)) {
+    return <i className={`${icon} mr-1`} />
   }
 
   return <span className='mr-1'>{icon}</span>
