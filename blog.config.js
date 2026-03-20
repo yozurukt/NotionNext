39dffc4f3fb515f58fcdddff1f6a478393fea02c
yozuru <d1322325603@gmail.com>
2026-03-21 02:37:50 +0800

    fix: 修复icon不显示问题

--- blog.config.js
+++ blog.config.js
@@ -19,7 +19,7 @@ const BLOG = {
   BIO: process.env.NEXT_PUBLIC_BIO || '在世界中心呼唤爱的野兽。',
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://yozuru.cn',
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Yozuru, 博客',
-  BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/public/favicon.ico', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
+  BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/favicon.ico', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
   BEI_AN: process.env.NEXT_PUBLIC_BEI_AN || '', // 备案号 闽ICP备XXXXXX
   BEI_AN_LINK: process.env.NEXT_PUBLIC_BEI_AN_LINK || 'https://beian.miit.gov.cn/', // 备案查询链接，如果用了萌备等备案请在这里填写
   BEI_AN_GONGAN: process.env.NEXT_PUBLIC_BEI_AN_GONGAN || '', // 公安备案号，例如 '浙公网安备3xxxxxxxx8号'
